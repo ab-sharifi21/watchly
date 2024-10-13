38f5f6b06e2b5aab2869ae613022dd63c8ae02db
Abdullah Sharifi <ab.sharifi19@gmail.com>
2024-10-13 17:53:59 +0200

    Create functions  to get movie genres

---
+++ src/services/getOneMovieGenre.ts
@@ -0,0 +1,18 @@
+import { Genre } from '@/types/Types';
+
+export const getOneMovieGenres = (genreIds: number[], genres: Genre[]) => {
+  const movieGenres: Genre[] = [];
+
+  if (Array.isArray(genreIds) && genreIds.length > 0) {
+    const genreMap = new Map(genres.map((genre) => [genre.id, genre]));
+
+    genreIds.forEach((genreId: number) => {
+      const genre = genreMap.get(genreId);
+      if (genre) {
+        movieGenres.push(genre);
+      }
+    });
+  }
+
+  return movieGenres.slice(0, 2);
+};
