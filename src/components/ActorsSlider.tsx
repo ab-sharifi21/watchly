73d2953fd23f37eb58751e7e2f63c0c81451f2cd
Abdullah Sharifi <ab.sharifi19@gmail.com>
2024-10-29 15:10:48 +0100

    Change the styles of ActorsSlider

--- src/components/ActorsSlider.tsx
+++ src/components/ActorsSlider.tsx
@@ -1,8 +1,8 @@
 'use client';
 import { ActorDetails } from '@/types/Types';
 import Image from 'next/image';
-import React from 'react'
-import useEmblaCarousel from 'embla-carousel-react'
+import React from 'react';
+import useEmblaCarousel from 'embla-carousel-react';
 
 interface Props {
   actors: ActorDetails[];
@@ -12,18 +12,34 @@ export const ActorsSlider: React.FC<Props> = ({ actors }) => {
   const [emblaRef] = useEmblaCarousel({ loop: true, dragFree: true });
 
   return (
-    <div className="embla overflow-hidden my-8" ref={emblaRef}>
-      <div className="embla__container flex gap-3">
-      {actors.filter((actor) => actor.profile_path !== null).map((actor, index) => (
-          <div key={index} className='flex-none'>
-            <Image width={150} height={150} src={`https://image.tmdb.org/t/p/w342${actor.profile_path}`} alt={`${actor.name}'s photo`} className='w-[150px] h-[150px] object-cover rounded-full' />
-            <div className='w-full text-center'>
-            <p className='text-center text-sm font-semibold'>{actor.name}</p>
-            <p className='text-xs text-slate-400'>{actor.character}</p>
-            </div>
-          </div>
-        ))}
+    <section className="my-8">
+      <h3 className="px-2 text-xl font-semibold">Get to know the actors:</h3>
+      <div className="embla mt-4 overflow-hidden" ref={emblaRef}>
+        <div className="embla__container flex gap-5">
+          {actors
+            .filter((actor) => actor.profile_path !== null)
+            .map((actor, index) => (
+              <article
+                key={index}
+                className="flex flex-none flex-col items-center"
+              >
+                <Image
+                  width={150}
+                  height={150}
+                  src={`https://image.tmdb.org/t/p/w342${actor.profile_path}`}
+                  alt={`${actor.name}'s photo`}
+                  className="h-[150px] w-[150px] rounded-full object-cover"
+                />
+                <div className="w-full text-center">
+                  <p className="text-center text-sm font-semibold">
+                    {actor.name}
+                  </p>
+                  <p className="text-xs text-slate-400">{actor.character}</p>
+                </div>
+              </article>
+            ))}
+        </div>
       </div>
-    </div>
-  )
-}
+    </section>
+  );
+};
