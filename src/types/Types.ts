8257c222d2c73052cbea175f4a1d09c1d27ac0f2
Abdullah Sharifi <ab.sharifi19@gmail.com>
2024-10-29 15:11:24 +0100

    Add CrewDetails  interface

--- src/types/Types.ts
+++ src/types/Types.ts
@@ -109,3 +109,17 @@ export interface ActorDetails {
   credit_id: string;
   order: number;
 }
+
+export interface CrewDetails {
+  adult: boolean;
+  credit_id: string;
+  department: string;
+  gender: number;
+  id: number;
+  job: string;
+  known_for_department: string;
+  name: string;
+  original_name: string;
+  popularity: number;
+  profile_path: string;
+}
