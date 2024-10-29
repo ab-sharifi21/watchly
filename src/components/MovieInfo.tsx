d788579d5a9dfc18f1a3f1ac7e681bb75ab3431d
Abdullah Sharifi <ab.sharifi19@gmail.com>
2024-10-29 15:12:05 +0100

    Change MovieInfo component's styles

--- src/components/MovieInfo.tsx
+++ src/components/MovieInfo.tsx
@@ -1,12 +1,17 @@
 import { formatDate } from '@/lib/utils';
-import { Genre, MovieDetails, SeriesDetails } from '@/types/Types';
-import { CiCalendar } from 'react-icons/ci';
+import {
+  DetailedMovie,
+  Genre,
+  MovieDetails,
+  SeriesDetails,
+} from '@/types/Types';
+import { CiCalendar, CiClock2 } from 'react-icons/ci';
 import { MdOutlineStar } from 'react-icons/md';
 import { Trailer } from './MovieTrailer';
 import { getOneMovieGenres } from '@/services';
 
 interface MovieInfoProps {
-  data: MovieDetails | SeriesDetails;
+  data: MovieDetails | SeriesDetails | DetailedMovie;
   genres: Genre[];
   isSeries?: boolean;
 }
@@ -14,42 +19,64 @@ interface MovieInfoProps {
 export const MovieInfo = ({ data, genres, isSeries }: MovieInfoProps) => {
   const releaseDate = formatDate(
     !isSeries
-      ? (data as MovieDetails).release_date
+      ? ((data as MovieDetails) || (data as DetailedMovie)).release_date
       : (data as SeriesDetails).first_air_date,
   );
 
-  const movieGenres = getOneMovieGenres(data.genre_ids, genres);
+  const movieGenres = getOneMovieGenres(
+    ((data as MovieDetails) || (data as MovieDetails)).genre_ids,
+    genres,
+  );
+
   return (
-    <section className="absolute bottom-12 px-4 md:bottom-[20%] md:px-8 xl:bottom-1/4">
+    <section className="absolute bottom-12 px-4 md:bottom-20 md:px-8">
       <h2 className="text-2xl font-semibold lg:text-3xl">
         {!isSeries
           ? (data as MovieDetails).title
           : (data as SeriesDetails).name}
       </h2>
-      <article className="my-2 flex gap-2">
+
+      <div className="my-2 flex gap-2">
         <div className="flex place-items-center gap-1">
           <span className="rounded bg-primary-color px-1 font-semibold">
             IMDb
           </span>{' '}
-          <span className="flex place-items-center">
-            {data.vote_average.toFixed(1)} <MdOutlineStar className="h-3 w-3" />
+          <span className="flex place-items-center gap-1">
+            {data.vote_average.toFixed(1)}{' '}
+            <MdOutlineStar className="h-4 w-4 text-secondary-color" />
           </span>
           <span className="text-sm text-slate-400 lg:text-[1rem]">
             ({data.vote_count})
           </span>
         </div>
-        {movieGenres.map((genre) => (
-          <p key={genre.id} className="text-sm lg:text-[1rem]">
-            <span className="text-lg text-slate-400">|</span> {genre.name}
-          </p>
-        ))}
-      </article>
-      <p className="max-w-[400px] text-sm md:max-w-[650px] lg:max-w-[900px] lg:text-[16px]">
+        {movieGenres.length > 0
+          ? movieGenres.map((genre) => (
+              <p key={genre.id} className="text-sm lg:text-[1rem]">
+                <span className="text-lg text-slate-400">|</span> {genre.name}
+              </p>
+            ))
+          : genres.slice(0, 2).map((genre) => (
+              <p key={genre.id} className="text-sm lg:text-[1rem]">
+                <span className="text-lg text-slate-400">|</span> {genre.name}
+              </p>
+            ))}
+      </div>
+
+      <p className="max-w-[350px] text-sm md:max-w-[500px] lg:max-w-[800px] lg:text-[16px]">
         {data.overview}
       </p>
-      <p className="my-2 flex place-items-center gap-2 text-sm text-slate-400">
-        <CiCalendar className="text-normal h-5 w-5 text-white" /> {releaseDate}
-      </p>
+      <div className="my-2 flex flex-col gap-0 md:flex-row md:gap-4">
+        <span className="flex place-items-center gap-1 text-sm text-slate-400">
+          <CiCalendar className="text-normal h-5 w-5 text-primary-color" />{' '}
+          {releaseDate}
+        </span>
+        {(data as DetailedMovie).runtime && (
+          <span className="flex place-items-center gap-1 text-sm text-slate-400">
+            <CiClock2 className="text-normal h-5 w-5 text-primary-color" />{' '}
+            {(data as DetailedMovie).runtime} mins
+          </span>
+        )}
+      </div>
       <Trailer id={data.id} />
     </section>
   );
