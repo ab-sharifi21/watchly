944dde72a1e1d35c8538eb7ea4f6ba8f5c044942
Abdullah Sharifi <ab.sharifi19@gmail.com>
2024-11-05 12:27:50 +0100

    Add a black background color when the header is scrolled

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,30 @@
+'use client';
+import { useEffect, useState } from 'react';
 import { Menu, Button, SearchBox, Logo } from './index';
 
 export const Header = () => {
+  const [isScrolled, setIsScrolled] = useState<boolean>(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 0) {
+        setIsScrolled(true);
+      } else {
+        setIsScrolled(false);
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
-    <header className="fixed left-0 right-0 top-0 z-20 flex justify-between px-2 py-2 md:px-6 md:py-4">
+    <header
+      className={`fixed left-0 right-0 top-0 z-20 flex justify-between px-2 py-2 md:px-6 ${isScrolled && 'bg-black/70'} md:py-4`}
+    >
       <div className={`md:gap:12 lg:gap-18 flex place-items-center gap-6`}>
         <Logo height={27} width={27} />
         <Menu />
