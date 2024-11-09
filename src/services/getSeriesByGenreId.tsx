7cde880b2e12a48414d9991b7b3f5a3e063db251
Abdullah Sharifi <ab.sharifi19@gmail.com>
2024-11-09 12:57:55 +0100

    Create getSeriesByGenreId function

---
+++ src/services/getSeriesByGenreId.tsx
@@ -0,0 +1,11 @@
+import { SeriesDetails } from '@/types/Types';
+import { fetcher } from './fetcher';
+
+export const getSeriesByGenreId = (
+  genreId: number,
+  page?: number,
+): Promise<{ results: SeriesDetails[] }> => {
+  const path = 'discover/tv';
+  const query = `&with_genres=${genreId}`;
+  return fetcher({ path, page, query });
+};
