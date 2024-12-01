a01859a01488e99010478f659fe6d3441c73c6a0
Abdullah Sharifi <ab.sharifi19@gmail.com>
2024-12-01 13:20:24 +0100

    Style the page scrollbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${poppins.className} flex min-h-screen flex-col bg-primary-bg-color text-white antialiased`}
+        className={`${poppins.className} flex min-h-screen flex-col bg-primary-bg-color text-white antialiased custom-scrollbar`}
       >
         <Header />
         <main className="flex-1">{children}</main>
