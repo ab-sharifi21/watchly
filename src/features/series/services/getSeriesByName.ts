f918cf07f8a4b85c27e57afeb275ab7a54229c57
Abdullah Sharifi <ab.sharifi19@gmail.com>
2026-02-05 18:35:18 +0100

    refactor: move all services related to TV shows to features/series/services

--- src/features/series/services/getSeriesByName.ts
+++ src/features/series/services/getSeriesByName.ts
@@ -1,4 +1,4 @@
-import { fetcher } from '../fetcher';
+import { fetcher } from '@/services//fetcher';
 
 export const getSeriesByName = ({
   path,
--- src/features/series/services/getSeriesInfoById.ts
+++ src/features/series/services/getSeriesInfoById.ts
@@ -1,4 +1,4 @@
-import { fetcher } from '../fetcher';
+import { fetcher } from '@/services/fetcher';
 
 export const getSeriesInfoById = (id: string) => {
   const path = `tv/${id}`;
